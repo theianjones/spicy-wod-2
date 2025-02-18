668097134541dc4e0ade7ad6a98f865bd8058005
Zac Jones <zacjones93@gmail.com>
2025-02-18 15:16:36 -0700

    fix: remove roundsToScore and reps per round

--- app/components/workouts/workout-card.tsx
+++ app/components/workouts/workout-card.tsx
@@ -40,29 +40,13 @@ function WorkoutCard({ workout }: WorkoutCardProps) {
           </div>
 
           <div className="space-y-1">
-            <CardDescription className="text-sm text-blac font-semibold">
+            <CardDescription className="text-sm text-black font-semibold whitespace-pre-wrap">
               {workout.description}
             </CardDescription>
           </div>
         </CardHeader>
 
-        <CardContent className=" p-6 space-y-4">
-          {(Boolean(workout.roundsToScore) || Boolean(workout.repsPerRound)) && (
-            <div className="grid grid-cols-2 gap-4">
-              {workout.repsPerRound && (
-                <div className="space-y-1">
-                  <div className="uppercase text-xs font-bold tracking-wide">REPS PER ROUND</div>
-                  <div className="text-sm">{workout.repsPerRound}</div>
-                </div>
-              )}
-              {workout.roundsToScore && (
-                <div className="space-y-1">
-                  <div className="text-sm">{workout.roundsToScore}</div>
-                </div>
-              )}
-            </div>
-          )}
-        </CardContent>
+
         <CardFooter className="flex justify-between">
           <div>
             {workout.movements && workout.movements.length > 0 && (
